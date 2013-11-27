c23c8982d5a7f0392d7c13b0d16344d0437cc237
lukasheinrich <lukas@lukasmba-2.local>
2013-11-27 02:17:13 -0500

    port

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var http = require("http");
 
+var port = process.env.PORT || 5000;
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World, this is a Jeu de Taquin");
   response.end();
-}).listen(8888);
+}).listen(port);
