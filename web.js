3ee11ee94e37ef2d7c8292bed8ab44a94cdf75a8
lukasheinrich <lukas@lukasmba-2.local>
2013-11-29 16:43:24 -0500

    first attempts

--- web.js
+++ web.js
@@ -1,9 +1,5 @@
-var http = require("http");
+var express = require('express');
+var app = express();
 
-var port = process.env.PORT || 5000;
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World, this is a Jeu de Taquin");
-  response.end();
-}).listen(port);
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000);
