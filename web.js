9077b5e857e9e7c64a96771248a677ce81b9b6bb
lukasheinrich <lukas@172-26-31-55.dynapool.nyu.edu>
2013-12-03 17:38:31 -0500

    v0.1

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static("young.html"));
+app.use(express.static(__dirname+"/young.html"));
 app.listen(process.env.PORT || 3000);
